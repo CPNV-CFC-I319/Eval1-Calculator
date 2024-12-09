9a87fde83a1314c2c54bce5f50f64f207cb4c569
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2024-12-09 11:06:50 +0100

    feat: update MathLib add to pass the test

--- src/MathLib.py
+++ src/MathLib.py
@@ -2,5 +2,5 @@ class MathLib:
 
     @classmethod
     def execute(self, mathRequest):
-        if(mathRequest.get_oper() == '+'):
+        if(mathRequest.get_oper() == 'add'):
             mathRequest.set_res(mathRequest.get_ope1() + mathRequest.get_ope2())
